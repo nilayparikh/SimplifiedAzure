772e7012b3ae58d0387bffff40952f5403763388
Nilay Parikh <nilay.parikh@ergosum.in>
2017-04-02 00:07:00 +0100

    license and other updates.

--- KeyVault/KeyVault/Keys/Key.cs
+++ KeyVault/KeyVault/Keys/Key.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿#region copyright
+/*
+Copyright (c) 2017 Nilay Parikh
+Modifications Copyright (c) 2017 Nilay Parikh
+B: https://blog.nilayparikh.com E: me@nilayparikh.com G: https://github.com/nilayparikh/
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using EasyAzure.KeyVault.Client;
--- KeyVault/KeyVault/Secrets/SecureSecret.cs
+++ KeyVault/KeyVault/Secrets/SecureSecret.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Runtime.Remoting.Channels;
-using System.Text;
-using System.Threading;
-using EasyAzure.KeyVault.Client;
-using EasyAzure.KeyVault.Keys;
+﻿#region copyright
+/*
+Copyright (c) 2017 Nilay Parikh
+Modifications Copyright (c) 2017 Nilay Parikh
+B: https://blog.nilayparikh.com E: me@nilayparikh.com G: https://github.com/nilayparikh/
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
 using System.Threading.Tasks;
+using EasyAzure.KeyVault.Client;
 using EasyAzure.KeyVault.Helper;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
-using Microsoft.Azure.KeyVault.WebKey;
 
 namespace EasyAzure.KeyVault.Secrets
 {
@@ -16,12 +24,11 @@ namespace EasyAzure.KeyVault.Secrets
     {
         private readonly IClient _client;
         private readonly string _secretId;
-        private readonly string _secureSecretId;
         private readonly string _secureKeyId;
+        private readonly string _secureSecretId;
 
-        private readonly Lazy<IKey> _key;
-
-        public SecureSecret(IClient client, string secretId, string secureSecretId, string secureKeyId) : base(client, secretId)
+        public SecureSecret(IClient client, string secretId, string secureSecretId, string secureKeyId)
+            : base(client, secretId)
         {
             if (client == null) throw new ArgumentNullException(nameof(client));
             if (secretId == null) throw new ArgumentNullException(nameof(secretId));
@@ -32,8 +39,6 @@ namespace EasyAzure.KeyVault.Secrets
             _secretId = secretId;
             _secureSecretId = secureSecretId;
             _secureKeyId = secureKeyId;
-
-            _key = new Lazy<IKey>(() => new Key(_client, _secureKeyId), LazyThreadSafetyMode.ExecutionAndPublication);
         }
 
         #region GetSecureSecret
