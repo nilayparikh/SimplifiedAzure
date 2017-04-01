848ac805f704df40d67e7d517a9446e3ecc2d787
Nilay Parikh <nilay.parikh@ergosum.in>
2017-04-01 05:24:05 +0100

    root change.

---
+++ KeyVault/KeyVault/Key.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Azure.KeyVault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+
+namespace EasyAzure.KeyVault
+{
+    public class Key : IKey
+    {
+        private readonly IClient _client;
+        private readonly string _keyId;
+
+        /// <summary>
+        /// Create an instance of key
+        /// </summary>
+        /// <param name="client">EasyAzure.KeyVault Client</param>
+        /// <param name="keyId">KeyId</param>
+        public Key(IClient client, string keyId)
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            if (keyId == null) throw new ArgumentNullException(nameof(keyId));
+
+            _client = client;
+            _keyId = keyId;
+        }
+
+        #region GetKey
+
+        /// <summary>
+        /// Gets the specified key
+        /// </summary>
+        /// <returns> retrieved key bundle </returns>
+        public async Task<KeyBundle> GetKeyAsync()
+        {
+            return await _client.KeyVaultClient.GetKeyAsync(_keyId);
+        }
+
+        /// <summary>
+        /// Gets the specified key
+        /// </summary>
+        /// <returns>Retrieved key bundle </returns>
+        public KeyBundle GetKey()
+        {
+            return GetKeyAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region WrapKey
+
+        public async Task<KeyOperationResult> WrapAsync(string symmetricKey, string algorithm)
+        {
+            return await WrapAsync(Encoding.UTF8.GetBytes(symmetricKey), algorithm);
+        }
+
+        public async Task<KeyOperationResult> WrapAsync(byte[] symmetricKey, string algorithm)
+        {
+            if (symmetricKey == null) throw new ArgumentNullException(nameof(symmetricKey));
+            if (algorithm == null) throw new ArgumentNullException(nameof(algorithm));
+
+            return await _client.KeyVaultClient.WrapKeyAsync(_keyId, algorithm, symmetricKey);
+        }
+
+        public KeyOperationResult Wrap(string symmetricKey, string algorithm)
+        {
+            return
+                WrapAsync(Encoding.UTF8.GetBytes(symmetricKey), algorithm)
+                    .ConfigureAwait(false)
+                    .GetAwaiter()
+                    .GetResult();
+        }
+
+        public KeyOperationResult Wrap(byte[] symmetricKey, string algorithm)
+        {
+            return WrapAsync(symmetricKey, algorithm).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region UnwarpKey
+
+        public async Task<KeyOperationResult> UnwrapAsync(byte[] wrappedKey, string algorithm)
+        {
+            if (wrappedKey == null) throw new ArgumentNullException(nameof(wrappedKey));
+            if (algorithm == null) throw new ArgumentNullException(nameof(algorithm));
+
+            return await _client.KeyVaultClient.UnwrapKeyAsync(_keyId, algorithm, wrappedKey);
+        }
+
+        public KeyOperationResult Unwrap(byte[] wrappedKey, string algorithm)
+        {
+            return UnwrapAsync(wrappedKey, algorithm).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Encrypt
+
+        public async Task<KeyOperationResult> EncryptAsync(string algorithm, byte[] plainText)
+        {
+            if (algorithm == null) throw new ArgumentNullException(nameof(algorithm));
+            if (plainText == null) throw new ArgumentNullException(nameof(plainText));
+
+            return await _client.KeyVaultClient.EncryptAsync(_keyId, algorithm, plainText);
+        }
+
+        public KeyOperationResult Encrypt(string algorithm, byte[] plainText)
+        {
+            return EncryptAsync(algorithm, plainText).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Decrypt
+
+        public async Task<KeyOperationResult> DecryptAsync(string algorithm, byte[] cipherText)
+        {
+            if (algorithm == null) throw new ArgumentNullException(nameof(algorithm));
+            if (cipherText == null) throw new ArgumentNullException(nameof(cipherText));
+
+            return await _client.KeyVaultClient.DecryptAsync(_keyId, algorithm, cipherText);
+        }
+
+        public KeyOperationResult Decrypt(string algorithm, byte[] cipherText)
+        {
+            return DecryptAsync(algorithm, cipherText).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Sign
+
+        public async Task<KeyOperationResult> SignAsync(string algorithm, byte[] digest)
+        {
+            if (algorithm == null) throw new ArgumentNullException(nameof(algorithm));
+            if (digest == null) throw new ArgumentNullException(nameof(digest));
+
+            return await _client.KeyVaultClient.SignAsync(_keyId, algorithm, digest);
+        }
+
+        public KeyOperationResult Sign(string algorithm, byte[] digest)
+        {
+            return SignAsync(algorithm, digest).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Verify
+
+        public async Task<bool> VerifyAsync(string algorithm, byte[] digest, byte[] signature)
+        {
+            if (algorithm == null) throw new ArgumentNullException(nameof(algorithm));
+            if (digest == null) throw new ArgumentNullException(nameof(digest));
+            if (signature == null) throw new ArgumentNullException(nameof(signature));
+
+            return await _client.KeyVaultClient.VerifyAsync(_keyId, algorithm, digest, signature);
+        }
+
+        public bool Verify(string algorithm, byte[] digest, byte[] signature)
+        {
+            return VerifyAsync(algorithm, digest, signature).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
